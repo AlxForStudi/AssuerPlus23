a666f1657e33854e6c27db99af0457439139ccae
Alexis Ferrand <alxferrand@yahoo.fr>
2023-02-13 11:04:18 +0100

    Initialisation projet

---
+++ src/main/java/com/example/asserplus23/AsserPlus23Application.java
@@ -0,0 +1,13 @@
+package com.example.asserplus23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsserPlus23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsserPlus23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/asserplus23/AsserPlus23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asserplus23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsserPlus23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
